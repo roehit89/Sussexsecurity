d12d409a5c2aa95687fe3cc71c67315559d19e6b
roehit89 <rohit.110689@gmail.com>
2015-10-24 20:22:40 -0400

    Revert "added log.i hello"

--- app/src/main/java/com/example/rohit/sussexsecurity/MainActivity.java
+++ app/src/main/java/com/example/rohit/sussexsecurity/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Log.i("hello","hello");
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
